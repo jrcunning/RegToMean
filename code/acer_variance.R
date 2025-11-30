b6745dc495ecc77448a254639a5c76207129ee11
Cunning <ross.cunning@gmail.com>
2025-11-29 18:40:46 -0600

    compute measurement error variance / k

---
+++ code/acer_variance.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+# Acer TLE data
+dat2<-read.csv(file="data/Acer3DMorphologyData.csv",stringsAsFactors=TRUE,header=TRUE)
+
+# Log-transform and calculate measurement error variance
+
+# Based on differences in measurements
+millsumm <- as.tibble(dat2) %>%
+  select(T0_TLE, T0_FieldTLE) %>%
+  mutate(across(everything(), log),
+         diff = T0_TLE - T0_FieldTLE,
+         subj = factor(row_number()))
+
+var(millsumm$diff) / 2
+
